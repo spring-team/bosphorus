eea003881af3bfdc080074c7fa8af520b065c974
Rod Johnson <johnsonroda@gmail.com>
2018-02-14 05:14:10 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/bosphorus/BosphorusApplication.java
@@ -0,0 +1,12 @@
+package com.atomist.bosphorus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BosphorusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BosphorusApplication.class, args);
+	}
+}
